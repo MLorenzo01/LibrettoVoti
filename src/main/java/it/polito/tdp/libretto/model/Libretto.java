3d6c8858231ef71c26933641fd8c0439e04cc8e1
loren <loren@172.21.50.35>
2023-03-06 11:24:23 +0100

    punti 1, 2, 3, 4

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,5 +1,69 @@
 package it.polito.tdp.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	private List<Voto> voti;
+
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	/**
+	 * Aggiungi un nuovo voto al libretto
+	 * (per ora non fa nessun controllo)
+	 * @param v il voto da aggiungere
+	 * @return true
+	 */
+	public boolean add(Voto v) {
+		return voti.add(v); //delega cieca, senza controlli
+		
+	}
+	/*public void add(String corso, int punti, LocalDate data) {
+		
+	}*/ // non è consigliato, perché se un giorno vorrei salvare le volte che provo un esame, 
+	   // dovrei cambiare anche questa classe, mentre con l'altro modifico solo la classe voto
+	  //  devo evitare di avere delle dipendenze, almeno faccio meno modifiche possibili
+	
+	public void stampa() {
+		for(Voto v: this.voti) {
+			System.out.println(v);
+		}
+	}
+	public List<Voto> getVoti(){
+		return this.voti;
+	}
+	public void stampaPuntiUguali(int valore) {
+		for(Voto v: this.voti) {
+			if(v.getPunti() == valore)
+				System.out.println(v);
+		}
+	}
+	public Voto CercaVotoPerNome(String corso) {
+		for(Voto v: this.voti) {
+			//if(v.getCorso().compareTo(corso) == 0) {
+			if(v.getCorso().equals(corso)) {
+				return v;
+			}
+		}
+		return null;
+		
+		//throw new RuntimeException("Voto non trovato");
+		// non conviene usare l'eccezione per un valore non trovato ma conviene usarla per cose più importanti
+	}
+	public boolean esisteVoto(Voto nuovo) {
+		for(Voto v: this.voti) {
+			//if(v.equalsCorsoPunti(nuovo)) si crea un metodo nell'altra classe solo nel caso in cui io uso questo 
+			//if in almeno 2 parti (refattorizzazione se faccio questa cosa in un secondo momento)
+			
+			if(v.getCorso().equals(nuovo.getCorso()) && v.getPunti() == nuovo.getPunti())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
 
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,9 +1,31 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Libretto lib = new Libretto();
+		
+		lib.add(new Voto("Analisi 1", 29, LocalDate.of(2021, 2, 15)));
+		lib.add(new Voto("Fisica 2", 21, LocalDate.of(2022, 6, 10)));
+		lib.add(new Voto("Informatica", 25, LocalDate.of(2021, 6, 10)));
+		
+		System.out.println("Stampa generale \n");
+		lib.stampa();
+		System.out.println("\nStampa tutti i voti uguali a 25 \n");
+		lib.stampaPuntiUguali(25);
+		Voto v = lib.CercaVotoPerNome("Analisi 1");
+		System.out.println("\nCerca voto per nome");
+		System.out.println(v);
+		
+		Voto a1bis = new Voto("Analisi 1", 29, LocalDate.of(2025, 2, 15));
+		Voto alter = new Voto("Analisi 1", 30, LocalDate.of(2025, 2, 15));
+		
+		System.out.println(a1bis + "è duplicato " + lib.esisteVoto(a1bis));
+		System.out.println(alter + "è duplicato " + lib.esisteVoto(alter));
+
+
 
 	}
 
